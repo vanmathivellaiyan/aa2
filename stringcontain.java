fff2b516b06660324251f99a515c3dac60d4df89
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-31 14:06:11 +0530

    Add files via upload

---
+++ stringcontain.java
@@ -0,0 +1,14 @@
+package guviprogram;
+
+public class stringcontain {
+	public static void main(String[] args){
+		String str=new String("bat");
+		String str1=new String("abate");
+		System.out.println(str);
+		System.out.println(str1);
+		if(str.contains(str1)){
+			System.out.println(str);
+		}
+	}
+
+}
